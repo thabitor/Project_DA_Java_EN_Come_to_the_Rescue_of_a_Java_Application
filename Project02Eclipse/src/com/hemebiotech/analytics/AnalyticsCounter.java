d8252fa0f27e76b31d9d78a5c78d8dcaf22f7ebc
thabitor <thabithamror@gmail.com>
2022-03-31 12:59:34 +0200

    semi-final code reading out file and frequency of each symptom

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,9 +1,6 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.*;
 import java.util.*;
 
 public class AnalyticsCounter {
@@ -11,29 +8,84 @@ public class AnalyticsCounter {
 //	private static int rashCount = 0;        // initialize to 0
 //	private static int pupilCount = 0;        // initialize to 0
 
-	public static void main(String args[]) throws Exception {
+	public static void main(String args[]) throws IOException {
 
 		BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"));
+
 		List<String> originalSymptomsFile = new ArrayList<String>();
 
-		String singleLine;	// this temporary variable refers to each line of the original text file
+		String singleLine;    // this temporary variable refers to each line of the original text file
 		while ((singleLine = reader.readLine()) != null) {
 			originalSymptomsFile.add(singleLine);
-			originalSymptomsFile.sort(String::compareToIgnoreCase);
 		}
+			reader.close();  // make sure this closing line is outside the while loop scope
 
-		reader.close();
-		System.out.println(originalSymptomsFile);
-		System.out.println("This array has " + originalSymptomsFile.size() + " values");
+		originalSymptomsFile.sort(String::compareToIgnoreCase);
 
 		ArrayList<String> uniqueSymptoms = new ArrayList<String>();
 
-		for (String element : originalSymptomsFile) {
+		for (String symptom : originalSymptomsFile) {
 			//'element' refers to each element of the uniqueSymptoms array; this statement says 'for each element in uniqueSymptoms reiterate through the originalSymptomsFile array and check the following condition/action
-			if (!uniqueSymptoms.contains(element)) {
-				uniqueSymptoms.add(element);
+			if (!uniqueSymptoms.contains(symptom)) {
+				uniqueSymptoms.add(symptom);
 			}
 		}
+
+		// these 4 lines below convert our original and unique ListArrays into Arrays
+
+			String[] arrOriginalSymptomsFile = new String[originalSymptomsFile.size()];
+			String[] arrUniqueSymptoms = new String[uniqueSymptoms.size()];
+			originalSymptomsFile.toArray(arrOriginalSymptomsFile);
+			uniqueSymptoms.toArray(arrUniqueSymptoms);
+
+			System.out.println(Arrays.toString(arrOriginalSymptomsFile) + "\n" + Arrays.toString(arrUniqueSymptoms));
+
+		int[] visitedArrOriginalSymptomsFile = new int [arrOriginalSymptomsFile.length];
+		int visited = -1;
+
+		for (int i = 0;i<arrOriginalSymptomsFile.length;i++) {
+			int counter = 1;
+			for (int j = i+1; j < arrOriginalSymptomsFile.length; j++) {
+				if (arrOriginalSymptomsFile[i].equals(arrOriginalSymptomsFile[j])) {
+					counter++;
+					visitedArrOriginalSymptomsFile[j] = visited;
+				}
+			}
+			if (visitedArrOriginalSymptomsFile[i] != visited) {
+				visitedArrOriginalSymptomsFile[i] = counter;
+			}
+		}
+		for (int i = 0; i < visitedArrOriginalSymptomsFile.length; i++) {
+			if (visitedArrOriginalSymptomsFile[i] != visited) {
+				System.out.println("Frequency of " + arrOriginalSymptomsFile[i] + " : " + visitedArrOriginalSymptomsFile[i]);
+			}
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+/*for (int i = 1; i <= originalSymptomsFile.size(); i++) {
+		for (String uniSymptom : originalSymptomsFile) {
+		if(uniqueSymptoms.contains(uniSymptom)) {
+		i++;
+		System.out.println(uniSymptom + " " + i);
+		}
+		}
+		}*/
+
+
+/*		System.out.println(originalSymptomsFile);
+		System.out.println("This array has " + originalSymptomsFile.size() + " values");
+
+
 
 		System.out.println("The unique symptoms from the original symptoms file are:\n" + uniqueSymptoms + "\nThat's " + uniqueSymptoms.size() + " symptoms with the following frequencies: ");
 
@@ -43,8 +95,11 @@ public class AnalyticsCounter {
 			// for each element in originalSymptomsFile reiterate through the new HashSet variable (newSet) and check the following condition/action
 			System.out.println(symptomElement + ": " + Collections.frequency(originalSymptomsFile, symptomElement));
 		}
-	}
-}
+
+
+*/
+//	}
+// }
 
 //		for (HashSet sym : newSet) {
 //			if(!newSet.contains(sym)) {
