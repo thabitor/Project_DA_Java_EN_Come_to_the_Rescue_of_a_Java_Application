39ad7ae95b1ecdcad401c5363e1385396524e41d
thabitor <thabithamror@gmail.com>
2022-04-01 16:34:57 +0200

    semi-final code with writer, alphabetically sorted file output

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,50 @@
 package com.hemebiotech.analytics;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.*;
 
 public class AnalyticsCounter {
-//	private static int headacheCount = 0;	// initialize to 0
-//	private static int rashCount = 0;		// initialize to 0
-//	private static int pupilCount = 0;		// initialize to 0
 	
 	public static void main(String args[]) throws Exception {
 		// first get input
 		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
+		String eachLine = reader.readLine();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
+		 List<String> symptomsDotText = new ArrayList<String>();
 
-			line = reader.readLine();	// get another symptom
+		while ((eachLine = reader.readLine()) != null) {
+			symptomsDotText.add(eachLine);
+			symptomsDotText.sort(String::compareToIgnoreCase);
 		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
+
+		reader.close();
+
+		String[] symptomsDotTxtToArray = new String [symptomsDotText.size()];
+		symptomsDotText.toArray(symptomsDotTxtToArray);
+	//	System.out.println(Arrays.toString(symptomsDotTxtToArray));
+
+		FileWriter writer = new FileWriter ("result.out", true);
+		int i = 0;
+
+		while (i<symptomsDotTxtToArray.length) {
+			int symptom = i;
+			while (i<symptomsDotTxtToArray.length && symptomsDotTxtToArray[i].equals(symptomsDotTxtToArray[symptom])) {
+				i++;
+			}
+			int count = i - symptom;
+			String symptomFrequency = symptomsDotTxtToArray[symptom].toString() + " : " + Integer.toString(count);
+			symptomFrequency = symptomFrequency.substring(0,1).toUpperCase() + symptomFrequency.substring(1).toLowerCase();
+			System.out.println(symptomFrequency);
+			writer.write(symptomFrequency + "\n");
+		}
 		writer.close();
 	}
 }
+
+// next generate output
+	/*	FileWriter writer = new FileWriter ("result.out");
+		writer.write("headache: " + headacheCount + "\n");
+		writer.write("rash: " + rashCount + "\n");
+		writer.write("dialated pupils: " + pupilCount + "\n");
+		writer.close();*/
