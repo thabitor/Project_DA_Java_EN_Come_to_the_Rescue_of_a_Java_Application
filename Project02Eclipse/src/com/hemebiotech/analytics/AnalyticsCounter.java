cf2911a7bfb6bee97388c43dd7d41d787bdbf96b
thabitor <thabithamror@gmail.com>
2022-04-05 10:09:39 +0200

    final code with writer, fixed counter number for 'fever'

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,33 +1,41 @@
 package com.hemebiotech.analytics;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.*;
 import java.util.*;
 
 public class AnalyticsCounter {
 	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));  // reads from original text file
-		String eachLine = reader.readLine();   //reads each line of the text file
-		List<String> symptomsDotText = new ArrayList<String>();  // new arraylist from the file
-		while ((eachLine = reader.readLine()) != null) {  // while loop to add each line of text file to the new arraylist
+
+//1st: Getting input from the original text file using BufferedReader class
+		BufferedReader reader = new BufferedReader (new FileReader("/home/thabit/Documents/Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application/Project02Eclipse/symptoms.txt"));  
+    String eachLine = new String();   //Reads each line of the text file
+		ArrayList<String> symptomsDotText = new ArrayList<String>();  //Creating new ArrayList for containing lines from the file as elements
+		while ((eachLine = reader.readLine()) != null) {  //While loop to add each line of text file to the new ArrayList
 			symptomsDotText.add(eachLine);
-			symptomsDotText.sort(String::compareToIgnoreCase); // sorting elements of arraylist (symptoms) alphabetically
+		Collections.sort(symptomsDotText); 
 		} reader.close();
-		String[] symptomsDotTxtToArray = new String [symptomsDotText.size()];  // converting ArrayList to Array
+
+//2nd: Convert the ArrayList into an Array
+    String[] symptomsDotTxtToArray = new String [symptomsDotText.size()];
 		symptomsDotText.toArray(symptomsDotTxtToArray);
-		//	System.out.println(Arrays.toString(symptomsDotTxtToArray));
-		FileWriter writer = new FileWriter ("result.out", true);      // introducing filewriter class for later file output using the append option
-		int i = 0;
-		while (i<symptomsDotTxtToArray.length) {  // double while loop to reiterate through elements of the array and count occurences
-			int symptom = i;
-			while (i<symptomsDotTxtToArray.length && symptomsDotTxtToArray[i].equals(symptomsDotTxtToArray[symptom])) {i++;}
-			// writing output of the program and to the output.txt
-			int count = i - symptom;
-			String symptomFrequency = symptomsDotTxtToArray[symptom].toString() + " : " + Integer.toString(count);
-			symptomFrequency = symptomFrequency.substring(0,1).toUpperCase() + symptomFrequency.substring(1).toLowerCase();
-			System.out.println(symptomFrequency);
-			writer.write(symptomFrequency + "\n");
+		System.out.println(Arrays.toString(symptomsDotTxtToArray));
+	 
+//3rd: Introduce BufferedWriter class for later file output using the append option
+    BufferedWriter writer = new BufferedWriter (new FileWriter("/home/thabit/Documents/Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application/Project02Eclipse/result.txt", true));    
+    
+    
+//4th: Introduce nested while loop to reiterate through the elements of the Array and count occurences of each
+   int lengthOfEachElement = 0;
+   while (lengthOfEachElement < symptomsDotTxtToArray.length) { 			int indexElement = lengthOfEachElement;
+	 while (lengthOfEachElement < symptomsDotTxtToArray.length && symptomsDotTxtToArray[lengthOfEachElement].equals(symptomsDotTxtToArray[indexElement])) {lengthOfEachElement++;}
+		
+   //count = length position - index position
+     			int count = lengthOfEachElement - indexElement; 
+      		 
+//5th: Writing output of the program and to the output.txt
+          System.out.println(symptomsDotTxtToArray[indexElement] + " : " + count);
+          String symptomFrequency = symptomsDotTxtToArray[indexElement].toString() + " : " + Integer.toString(count);
+			    symptomFrequency = symptomFrequency.substring(0,1).toUpperCase() + symptomFrequency.substring(1).toLowerCase();
+			    writer.write(symptomFrequency + "\n");
 		} writer.close();
-	}
+  }
 }
