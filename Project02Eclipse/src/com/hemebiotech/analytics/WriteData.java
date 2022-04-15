43167b8d1c2f3f10459cab045278dd9bd4b746d7
thabitor <thabithamror@gmail.com>
2022-04-16 00:09:23 +0200

    minor modification in writer

--- Project02Eclipse/src/com/hemebiotech/analytics/WriteData.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteData.java
@@ -23,9 +23,13 @@ public class WriteData implements ISymptomWriter {
 		FileWriter writer = new FileWriter ("analytics/results.out");
 	try {for (Map.Entry<String, Integer> key : counterResult.entrySet()){
 		 if (key.getValue() == 1) {
-						writer.write(key.getKey().substring(0, 1).toUpperCase() + key.getKey().substring(1) + " occurs " + key.getValue() + " time.\n"); 
+						writer.write(key.getKey().substring(0, 1).toUpperCase() + 
+						key.getKey().substring(1) + " occurs " + 
+						key.getValue() + " time.\n"); 
 				} else {
-						writer.write(key.getKey().substring(0, 1).toUpperCase() + key.getKey().substring(1) + " occurs " + key.getValue() + " times.\n");
+						writer.write(key.getKey().substring(0, 1).toUpperCase() + 
+						key.getKey().substring(1) + " occurs " + 
+						key.getValue() + " times.\n");
 					} writer.flush();  
 				} writer.close(); 
 	} catch (IOException e) {
